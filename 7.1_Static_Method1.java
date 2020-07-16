59350d00ba72fadff58073cf759d38e15f8549df
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:05:35 +0530

    Add files via upload

---
+++ 7.1_Static_Method1.java
@@ -0,0 +1,16 @@
+package method;
+
+public class Static_Method1
+{
+
+	public static void main(String[] args) 
+	{
+          disp();
+	}
+	static void disp()
+	{
+		int x=10;
+		System.out.println("value of x: "+x);
+	}
+
+}
